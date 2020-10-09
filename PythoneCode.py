e83095489810cdcf1b06c24a28a846f280e32ece
WajithAAA <abdul.17@business.mrt.ac.lk>
2020-10-09 14:50:17 -0500

    Python code file

---
+++ PythoneCode.py
@@ -0,0 +1,47 @@
+#If statement using for tecket printing
+
+age = int(input ("what is your age:"))
+if age > 50:
+   t_price = 50
+elif age > 25:
+   t_price = 25
+elif age > 18:
+    t_price = 10
+else:
+    t_price = 0
+print ("ticket price is :", t_price)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
