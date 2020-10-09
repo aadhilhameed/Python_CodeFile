97c88098bc7cbfd88184828a05162403586600cc
Mohamed Wajid <66938837+WajithAAA@users.noreply.github.com>
2020-10-09 15:07:58 -0500

    Update PythoneCode.py

--- PythoneCode.py
+++ PythoneCode.py
@@ -1,16 +1,23 @@
 #If statement using for tecket printing
+print("------WELCOME---------")
+name = input("What is your name ?")
+age = int(input ("what is your age ?"))
 
-age = int(input ("what is your age:"))
 if age > 50:
    t_price = 50
+   
 elif age > 25:
    t_price = 25
+   
 elif age > 18:
     t_price = 10
+      
 else:
     t_price = 0
+
 print ("ticket price is :", t_price)
 
+print("-------THANK YOU----------")
 
 
 
